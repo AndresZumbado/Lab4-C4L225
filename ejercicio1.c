365b4df1b86447be1a5675c3b310bd63012aa70f
AndresZumbado <andreszm115@hotmail.com>
2025-10-21 20:33:35 -0600

    Se le agrego al codigo del ejercicio1 que pudiera convertir una matriz e un arreglo unidimensional usando solamente puntero. El codigo funciona correctamente y no tiene ningun memory leak ademas de que corre perfecto sin warnings.

--- ejercicio1.c
+++ ejercicio1.c
@@ -5,16 +5,17 @@
 #define COLUMNAS 4
 
 int *unidimensionalizar(int **matriz) {
-    int array = (int*)malloc (FILAS * COLUMNAS  * sizeof(int));
+    int *array = (int*)malloc (FILAS * COLUMNAS  * sizeof(int));
     int cont = 0;
     for(int i = 0; i < FILAS; i++) {
 	for(int j = 0; j < COLUMNAS; j++) {
-	    *(array + cont) = *(*(matriz + j) + i);
-	    cont = cont++;
+	    *(array + cont) = *(*(matriz + i) + j);
+	    cont++;
 	};
     };
     return array;
 };
+
 
 int main() {
 
@@ -25,18 +26,36 @@ int main() {
     {2,0,5,0}
     };
 
-    int *ptr_matriz = (int **) malloc(FILAS * int);
+    int **ptr_matriz = (int **) malloc(FILAS * sizeof(int*));
+    
+    if (ptr_matriz == NULL){
+	return 1;
+    };
+
+    for(int i = 0; i < FILAS; i++) {
+	*(ptr_matriz + i) = (int *)malloc(COLUMNAS * sizeof(int));
+	if (*(ptr_matriz + i) == NULL){
+	    return 2;
+	};
+    };
+
     
     for(int i = 0; i < FILAS; i++) {
-	ptr_matriz + i = (int *)malloc(COLUMNAS * int);
-    };
-    for(int i = 0; i < FILAS; i++) {
         for(int j = 0; j < COLUMNAS; j++) {
-            *(*(ptr_matriz + j) + i) = matriz[i][j];
-
-    int ptr_array = unidimensionalizar(ptr_matriz);
+            *(*(ptr_matriz + i) + j) = matriz[i][j];
+	};
+    };
+    int *ptr_array = unidimensionalizar(ptr_matriz);
     for(int i = 0; i < FILAS * COLUMNAS; i++) {
 	printf(" %d ", *(ptr_array + i));
     };
+    
+    for(int i = 0; i < FILAS; i++) {
+	free(*(ptr_matriz + i));
+    };
+    free(ptr_matriz);
+    free(ptr_array);
+    
 }
+
 
